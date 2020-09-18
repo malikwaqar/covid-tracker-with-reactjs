0a09bcccba638b9315ad8c4a6255d97a481c7e81
Malik Waqar Ul Hassan <malikwaqarofficial@gmail.com>
2020-09-18 12:46:43 +0500

    Countries Table Sorted by Cases

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import { MenuItem, FormControl, Select} from '@material-ui/core';
+import { MenuItem, FormControl, Select, Card, CardContent} from '@material-ui/core';
+import InfoBox from './InfoBox';
+import Map from './Map';
+import Table from './Table';
+import { sortData } from './util';
 
 function App() {
   const [countries, setCountries] = useState([]);
   const[country, setCountry] = useState('worldwide');
+  const[countryInfo, setCountryInfo] = useState({});
+  const[tableData, setTableData] = useState([]);
   // https://disease.sh/v3/covid-19/countries
+  
+  useEffect(() => {
+    fetch("https://disease.sh/v3/covid-19/all")
+    .then(response => response.json())
+    .then(data => {
+      setCountryInfo(data);
+    })
+  }, []);
   useEffect(() => {
     const getCountriesData = async () => {
       await fetch("https://disease.sh/v3/covid-19/countries")
@@ -17,17 +31,30 @@ function App() {
             value: country.countryInfo.iso2,
           }
         ));
+        const sortedData = sortData(data);
+        setTableData(sortedData);
         setCountries(countries);
-      })
-    }
+      });
+    };
     getCountriesData();
   }, []);
+
   const onCountryChange = async (event) => {
     const countryCode = event.target.value;
     setCountry(countryCode);
-  }
+    const url = countryCode === 'worldwide' ? 'https://disease.sh/v3/covid-19/all' : `https://disease.sh/v3/covid-19/countries/${countryCode}`;
+
+    await fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      setCountry(countryCode);
+      setCountryInfo(data);
+    });
+  };
+  // console.log("Country Info", countryInfo);
   return (
     <div className="app">
+      <div className="app__left">
       <div className="app__header">
       <h1>COVID-19 TRACKER</h1>
       <FormControl className="app__dropdown">
@@ -39,6 +66,22 @@ function App() {
         </Select>
       </FormControl>
       </div>
+      <div className="app__stats">
+        <InfoBox title="Coronavirus Cases" cases={countryInfo.todayCases} total={countryInfo.cases} />
+        <InfoBox title="Recovered" cases={countryInfo.todayRecovered} total={countryInfo.recovered} />
+        <InfoBox title="Deaths" cases={countryInfo.todayDeaths} total={countryInfo.deaths} />
+      </div>
+      <Map />
+      </div>
+      <Card className="app__right">
+        <CardContent>
+          <h3>Live Cases by Country</h3>
+          <Table countries={tableData} />
+          <h3>Worldwide New Cases</h3>
+        </CardContent>
+        {/* Table */}
+        {/* Graph */}
+      </Card>
     </div>
   );
 }
