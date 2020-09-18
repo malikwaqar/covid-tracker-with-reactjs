3352ef21e326f5fb04b2d8625dbcd58832e990f8
Malik Waqar Ul Hassan <malikwaqarofficial@gmail.com>
2020-09-18 16:18:22 +0500

    Line Graph Component Completed

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { MenuItem, FormControl, Select, Card, CardContent} from '@material-ui/co
 import InfoBox from './InfoBox';
 import Map from './Map';
 import Table from './Table';
+import LineGraph from './LineGraph';
 import { sortData } from './util';
 
 function App() {
@@ -78,6 +79,7 @@ function App() {
           <h3>Live Cases by Country</h3>
           <Table countries={tableData} />
           <h3>Worldwide New Cases</h3>
+          <LineGraph />
         </CardContent>
         {/* Table */}
         {/* Graph */}
